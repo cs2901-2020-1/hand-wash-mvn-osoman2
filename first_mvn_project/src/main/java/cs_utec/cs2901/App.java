5aa7ca7cf8298edcd9bdae784d376fda066eb182
osman vilchez <osman.vilchez@utec.edu.pe>
2020-04-02 10:41:26 -0500

    firstversion

---
+++ first_mvn_project/src/main/java/cs_utec/cs2901/App.java
@@ -0,0 +1,16 @@
+package cs_utec.cs2901;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	for(int i=0;i<100;i++){
+		System.out.println("Me debo lavar las manos por 20seg \n");
+	}
+    }
+}
